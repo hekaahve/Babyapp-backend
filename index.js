069b48e38087be0e18df122fa301c2c8ae111413
Heini Ahven <heini.ahven@gmail.com>
2022-10-21 22:45:14 +0300

    schema changed

--- index.js
+++ index.js
@@ -1,87 +1,85 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Chart = require('./models/chart')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Chart = require("./models/chart");
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-
-app.get('/api/charts',(request, response) => {
-    Chart.find({}).then(charts => {
-      response.json(charts)
-    })
-  })
+app.get("/api/charts", (request, response) => {
+  Chart.find({}).then((charts) => {
+    response.json(charts);
+  });
+});
 //some problem with the fletch, fix
-  app.get('/api/charts/:name&:age', (request, response) => {
-    const name = String(request.params.name)
-    const age = String(request.params.age)
-    console.log(name)
+app.get("/api/charts/:name&:age", (request, response) => {
+  const name = String(request.params.name);
+  const age = Number(request.params.age);
+  console.log(name);
 
-    Chart.find(chart => chart.name === name && chart.age === age).then(chart =>{
-      console.log(chart)
-      if (chart){
-        response.json(chart)
-        console.log(chart)
+  Chart.find((chart) => chart.name === name && chart.age === age).then(
+    (chart) => {
+      console.log(chart);
+      if (chart) {
+        response.json(chart);
+        console.log(chart);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
-    }) 
-  })
+    }
+  );
+});
 
 //finds all matches by name
- app.get('/api/charts/:name',(request, response) => {
-    const name = String(request.params.name)
+app.get("/api/charts/:name", (request, response) => {
+  const name = String(request.params.name);
 
-    Chart.find({name}).then(charts => {
-      if (charts) {
-        response.json(charts)
-      } else {
-        response.status(404).end()
-      }
-      
-    })
-  })
-
-  app.post('/api/charts',(request, response, next) => {
-    const maxId = Math.floor(Math.random() * 100)
-    const body = request.body
-
-    if (!body) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+  Chart.find({ name }).then((charts) => {
+    if (charts) {
+      response.json(charts);
+    } else {
+      response.status(404).end();
     }
-    const chart = new Chart({
-      age: body.age,
-      name: body.name,
-      weight: body.weight
+  });
+});
+
+app.post("/api/charts", (request, response, next) => {
+  const maxId = Math.floor(Math.random() * 100);
+  const body = request.body;
+
+  if (!body) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const chart = new Chart({
+    age: body.age,
+    name: body.name,
+    weight: body.weight,
+  });
+
+  chart
+    .save()
+    .then((savedChart) => savedChart.toJSON())
+    .then((savedAndFormattedChart) => {
+      response.json(savedAndFormattedChart);
     })
-  
-  chart.save()
-    .then(savedChart => savedChart.toJSON())
-    .then(savedAndFormattedChart =>{
-      response.json(savedAndFormattedChart)
-    })
-    .catch(error => next(error))
+    .catch((error) => next(error));
+});
 
-    console.log(chart)
-    response.json(chart)
-  })
+app.delete("/api/charts/:id", (request, response) => {
+  const id = Number(request.params.id);
+  charts = charts.filter((chart) => chart.id !== id);
 
-  app.delete('/api/charts/:id', (request, response) => {
-    const id = Number(request.params.id)
-    charts = charts.filter(chart => chart.id !== id)
-  
-    response.status(204).end()
-  })
+  response.status(204).end();
+});
 
-  /*
+/*
   app.get('/api/charts/:age',(request, response) => {
     const age = String(request.params.age)
     const ages = charts.filter(chart => chart.age === age);
@@ -91,13 +89,13 @@ app.get('/api/charts',(request, response) => {
 //finds match for certain weight & certain name
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 //unknown errorhandling
-app.use(unknownEndpoint)
-  
-const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- models/chart.js
+++ models/chart.js
@@ -1,29 +1,30 @@
-const { default: mongoose } = require("mongoose")
+const { default: mongoose } = require("mongoose");
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const chartSchema = new mongoose.Schema({
-    age: String,
-    weight: Number, 
-    name: String,
-    })
+  age: Number,
+  weight: Number,
+  name: String,
+});
 
-chartSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
-    })
+chartSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Chart', chartSchema)
+module.exports = mongoose.model("Chart", chartSchema);
